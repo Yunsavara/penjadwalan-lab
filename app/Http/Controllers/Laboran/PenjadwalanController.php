fe1e43f89bb859a8e25cca27416b1a41e51af999
Yunsavara <yunikosatria23@gmail.com>
2025-02-19 18:51:20 +0700

    WIP: Form Create

--- app/Http/Controllers/Laboran/PenjadwalanController.php
+++ app/Http/Controllers/Laboran/PenjadwalanController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Laboran;
 
-use App\Http\Controllers\Controller;
+use App\Models\Penjadwalan;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Semester;
 
 class PenjadwalanController extends Controller
 {
@@ -11,6 +13,21 @@ class PenjadwalanController extends Controller
         return view('laboran.penjadwalan-prodi.penjadwalan-prodi', [
             'page_meta' => [
                 'page' => 'Generate Jadwal Prodi'
+            ]
+        ]);
+    }
+
+    // Note
+    // Hapus Status Selesai Pada Semester
+
+    public function create(){
+        return view("laboran.penjadwalan.form-penjadawalan", [
+            'Penjadawalan' => new Penjadwalan(),
+            'page_meta' => [
+                'page' => "Tambah Penjadwalan",
+                'method' => 'POST',
+                'url' => route('laboran.penjadwalan.create'),
+                'button_text' => 'Tambah Penjadwalan'
             ]
         ]);
     }
