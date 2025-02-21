b7475654ae62dbd72b2ec451d51b10896a711138
Yunsavara <yunikosatria23@gmail.com>
2025-02-21 07:52:58 +0700

    WIP: Delete Semester

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -36,10 +36,6 @@
                 </ul>
             </li>
 
-            <li class="sidebar-item {{ Route::is('admin.semester*') ? 'active' : '' }}">
-                <a href="{{ Route('admin.semester') }}" class="sidebar-link"><i data-feather="framer" class="sidebar-icon-link"></i>Semester</a>
-            </li>
-
             <li class="sidebar-item {{ Route::is('admin.matakuliah*') ? 'active' : '' }}">
                 <a href="{{ Route('admin.matakuliah') }}" class="sidebar-link"><i data-feather="book-open" class="sidebar-icon-link"></i>Mata Kuliah</a>
             </li>
